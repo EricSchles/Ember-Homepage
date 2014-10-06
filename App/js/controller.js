0c043e72dc27324064a43c215546a66aeb823e50
Freddie Andrade <faandrade@gmail.com>
2014-10-06 02:33:06 -0400

    init ember app

---
+++ App/js/controller.js
@@ -0,0 +1,4 @@
+App.BannerComponent = Ember.Component.extend({
+  banner: "img/videoFallback2.png",
+  statement: "Every language in the world"
+});
---
+++ App/js/router.js
@@ -0,0 +1,16 @@
+App.Router.map(function() {
+  this.route("index");
+  this.route("about");
+  this.route("team");
+  this.route("expeditions");
+  this.route("projects");
+  this.route("get_involved");
+  this.route("press");
+
+  this.route("translate", { path: "https://docs.google.com/forms/d/1hVjiergDRQouyebGRn7gpGguKfKITTqJ9ar82DweRmo/viewform"});
+  this.route("youtube", { path: "https://youtube.com/user/WikiTongues"});
+  this.route("facebook", { path: "https://www.facebook.com/wikitongues"});
+  this.route("twitter", { path: "https://twitter.com/wikitongues"});
+  this.route("tumblr", { path: "http://wikitongues.tumblr.com/"});
+  this.route("instagram", { path: "http://instagram.com/wikitongues"});
+});
