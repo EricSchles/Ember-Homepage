6c0f43d3ab24264cfe81eb7822a59baac8c80917
Freddie Andrade <faandrade@gmail.com>
2014-10-25 17:44:50 -0400

    team fixtures

--- App/js/controller.js
+++ App/js/controller.js
@@ -1,4 +1,4 @@
 App.BannerComponent = Ember.Component.extend({
-  banner: "img/videoFallback2.png",
+  image: "img/videoFallback2.png",
   statement: "Every language in the world"
 });
--- App/js/router.js
+++ App/js/router.js
@@ -1,4 +1,5 @@
 App.Router.map(function() {
+  this.route("index");
   this.route("about");
   this.route("team");
   this.route("expeditions");
@@ -9,3 +10,108 @@ App.Router.map(function() {
   this.route("volunteer");
   this.route("contact");
 });
+
+// // App.IndexRoute = Ember.Route.extend({
+// //   model: function() {
+// //     return {
+// //       image: "../img/videoFallback2.png",
+// //       statement: "Every language in the world"
+// //     };
+// //   }
+// // });
+
+// App.TeamRoute = Ember.Route.extend({
+//   model: function() {
+//     return this.store.findAll("volunt")
+//   }
+// });
+
+// App.Contact = DS.Model.extend({
+//   name: ("string"),
+//   location: ("string"),
+//   photo: ("string")
+// });
+
+// App.Contact.FIXTURES = [
+// {
+//   id:100,
+//   name: "Daniel Bogre Udell",
+//   location: "New York, USA",
+//   photo: "../img/faces/daniel.jpg"
+// },{
+//   id:101,
+//   name: "Freddie Andrade",
+//   location: "New York, USA",
+//   photo: "../img/faces/freddie.jpg"
+// },{
+//   id:102,
+//   name: "Lindie Botes",
+//   location: "Prætoria, South Africa",
+//   photo: "../img/faces/lindie.jpg"
+// },{
+//   id:103,
+//   name: "Cathy Zhang",
+//   location: "New York, USA",
+//   photo: "../img/faces/cathy.jpg"
+// },{
+//   id:104,
+//   name: "Pau Mateo",
+//   location: "Kaunas, Lithuania",
+//   photo: "../img/faces/pau.jpg"
+// },{
+//   id:105,
+//   name: "Manjusha Raveendran",
+//   location: "Buffalo, USA",
+//   photo: "../img/faces/manjusha.jpg"
+// },{
+//   id:106,
+//   name: "Madeleine Koerner",
+//   location: "Moscow, Russia",
+//   photo: "../img/faces/madeleine.jpg"
+// },{
+//   id:107,
+//   name: "Sarah Doyle",
+//   location: "Port Vila, Vanuatu",
+//   photo: "../img/faces/sarah.jpg"
+// },{
+//   id:108,
+//   name: "Plator Gashi",
+//   location: "Prishtina, Kosovo",
+//   photo: "../img/faces/plator.jpg"
+// },{
+//   id:109,
+//   name: "Teddy Nee",
+//   location: "Guishan, Taiwan",
+//   photo: "../img/faces/teddy.jpg"
+// },{
+//   id:110,
+//   name: "Maxi Salomone",
+//   location: "Bahia Blanca, Argentina",
+//   photo: "../img/faces/maxi.jpg"
+// },{
+//   id:111,
+//   name: "Hugo Campbell Sills",
+//   location: "Bordeaux, France",
+//   photo: "../img/faces/hugo.jpg"
+// },{
+//   id:112,
+//   name: "Yasmeen Ahmed",
+//   location: "Doha, Qatar",
+//   photo: "../img/faces/yasmeen.jpg"
+// },{
+//   id:113,
+//   name: "Luis Miguel Bercia",
+//   location: "Brussles, Belgium",
+//   photo: "../img/faces/luis.jpg"
+// },{
+//   id:114,
+//   name: "Andersson Causayá",
+//   location: "Santiago, Chile",
+//   photo: "../img/faces/andersson.jpg"
+// },{
+//   id:115,
+//   name: "Liam Neeve",
+//   location: "Edinburgh, Scotland",
+//   photo: "../img/faces/liam.jpg"
+// }
+// ]
