4b03a19a7d97b6a6b51e079e59d25974e71ad111
Freddie Andrade <faandrade@gmail.com>
2014-10-26 18:09:38 -0400

    emberified team and banners

--- App/js/app.js
+++ App/js/app.js
@@ -3,7 +3,7 @@ var App = Ember.Application.create({
 });
 
 App.Router.map(function() {
-  this.route("index");
+  // this.route("index");
   this.route("about");
   this.route("team");
   this.route("expeditions");
@@ -13,114 +13,144 @@ App.Router.map(function() {
   this.route("submit");
   this.route("volunteer");
   this.route("contact");
+  this.route("privacy");
+  this.route("terms");
 });
 
-App.BannerComponent = Ember.Component.extend({
-  image: "img/videoFallback2.png",
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return [
+      {
+        id: 0,
+        image: "img/videoFallback2.png",
+        text: "Every Language In The World"
+      }
+    ];
+  }
+});
+
+App.AboutRoute = Ember.Route.extend({
+  model: function() {
+    return [
+      {
+        id: 0,
+        image: "img/earth-02.jpg",
+        text: "Our World"
+      }
+    ];
+  }
+});
+
+App.TeamRoute = Ember.Route.extend({
+  model: function() {
+    return [
+      {
+        id:1,
+        name: "Daniel Bogre Udell",
+        location: "New York, USA",
+        photo: "img/faces/daniel.jpg"
+      },
+      {
+        id:2,
+        name: "Freddie Andrade",
+        location: "New York, USA",
+        photo: "img/faces/freddie.jpg"
+      },
+      {
+        id:3,
+        name: "Lindie Botes",
+        location: "Prætoria, South Africa",
+        photo: "img/faces/lindie.jpg"
+      },
+      {
+        id:4,
+        name: "Cathy Zhang",
+        location: "New York, USA",
+        photo: "img/faces/cathy.jpg"
+      },
+      {
+        id:5,
+        name: "Pau Mateo",
+        location: "Kaunas, Lithuania",
+        photo: "img/faces/pau.jpg"
+      },
+      {
+        id:6,
+        name: "Manjusha Raveendran",
+        location: "Buffalo, USA",
+        photo: "img/faces/manjusha.jpg"
+      },
+      {
+        id:7,
+        name: "Madeleine Koerner",
+        location: "Moscow, Russia",
+        photo: "img/faces/madeleine.jpg"
+      },
+      {
+        id:8,
+        name: "Sarah Doyle",
+        location: "Port Vila, Vanuatu",
+        photo: "img/faces/sarah.jpg"
+      },
+      {
+        id:9,
+        name: "Plator Gashi",
+        location: "Prishtina, Kosovo",
+        photo: "img/faces/plator.jpg"
+      },
+      {
+        id:10,
+        name: "Teddy Nee",
+        location: "Guishan, Taiwan",
+        photo: "img/faces/teddy.jpg"
+      },
+      {
+        id:11,
+        name: "Maxi Salomone",
+        location: "Bahia Blanca, Argentina",
+        photo: "img/faces/maxi.jpg"
+      },
+      {
+        id:12,
+        name: "Hugo Campbell Sills",
+        location: "Bordeaux, France",
+        photo: "img/faces/hugo.jpg"
+      },
+      {
+        id:13,
+        name: "Yasmeen Abdullahi",
+        location: "Doha, Qatar",
+        photo: "img/faces/yasmeen.jpg"
+      },
+      {
+        id:14,
+        name: "Luis Miguel Bercia",
+        location: "Brussles, Belgium",
+        photo: "img/faces/luis.jpg"
+      },
+      {
+        id:15,
+        name: "Andersson Causayá",
+        location: "Santiago, Chile",
+        photo: "img/faces/andersson.jpg"
+      },
+      {
+        id:16,
+        name: "Liam Neeve",
+        location: "Edinburgh, Scotland",
+        photo: "img/faces/liam.jpg"
+      }
+    ];
+  }
+});
+
+App.IndexView = Ember.View.extend({
+  image: "../img/videoFallback2.png",
   statement: "Every language in the world"
 });
 
-// App.IndexRoute = Ember.Route.extend({
-//   model: function() {
-//     return {
-//       image: "../img/videoFallback2.png",
-//       statement: "Every language in the world"
-//     };
-//   }
-// });
-
-// App.TeamRoute = Ember.Route.extend({
-//   model: function() {
-//     return this.store.findAll("volunt")
-//   }
-// });
-
-// App.Contact = DS.Model.extend({
-//   name: ("string"),
-//   location: ("string"),
-//   photo: ("string")
-// });
-
-// App.Contact.FIXTURES = [
-//   {
-//     id:100,
-//     name: "Daniel Bogre Udell",
-//     location: "New York, USA",
-//     photo: "../img/faces/daniel.jpg"
-//   },{
-//     id:101,
-//     name: "Freddie Andrade",
-//     location: "New York, USA",
-//     photo: "../img/faces/freddie.jpg"
-//   },{
-//     id:102,
-//     name: "Lindie Botes",
-//     location: "Prætoria, South Africa",
-//     photo: "../img/faces/lindie.jpg"
-//   },{
-//     id:103,
-//     name: "Cathy Zhang",
-//     location: "New York, USA",
-//     photo: "../img/faces/cathy.jpg"
-//   },{
-//     id:104,
-//     name: "Pau Mateo",
-//     location: "Kaunas, Lithuania",
-//     photo: "../img/faces/pau.jpg"
-//   },{
-//     id:105,
-//     name: "Manjusha Raveendran",
-//     location: "Buffalo, USA",
-//     photo: "../img/faces/manjusha.jpg"
-//   },{
-//     id:106,
-//     name: "Madeleine Koerner",
-//     location: "Moscow, Russia",
-//     photo: "../img/faces/madeleine.jpg"
-//   },{
-//     id:107,
-//     name: "Sarah Doyle",
-//     location: "Port Vila, Vanuatu",
-//     photo: "../img/faces/sarah.jpg"
-//   },{
-//     id:108,
-//     name: "Plator Gashi",
-//     location: "Prishtina, Kosovo",
-//     photo: "../img/faces/plator.jpg"
-//   },{
-//     id:109,
-//     name: "Teddy Nee",
-//     location: "Guishan, Taiwan",
-//     photo: "../img/faces/teddy.jpg"
-//   },{
-//     id:110,
-//     name: "Maxi Salomone",
-//     location: "Bahia Blanca, Argentina",
-//     photo: "../img/faces/maxi.jpg"
-//   },{
-//     id:111,
-//     name: "Hugo Campbell Sills",
-//     location: "Bordeaux, France",
-//     photo: "../img/faces/hugo.jpg"
-//   },{
-//     id:112,
-//     name: "Yasmeen Ahmed",
-//     location: "Doha, Qatar",
-//     photo: "../img/faces/yasmeen.jpg"
-//   },{
-//     id:113,
-//     name: "Luis Miguel Bercia",
-//     location: "Brussles, Belgium",
-//     photo: "../img/faces/luis.jpg"
-//   },{
-//     id:114,
-//     name: "Andersson Causayá",
-//     location: "Santiago, Chile",
-//     photo: "../img/faces/andersson.jpg"
-//   },{
-//     id:115,
-//     name: "Liam Neeve",
-//     location: "Edinburgh, Scotland",
-//     photo: "../img/faces/liam.jpg"
-//   }
-// ]
+App.Contact = DS.Model.extend({
+  name: ("string"),
+  location: ("string"),
+  photo: ("string")
+});
